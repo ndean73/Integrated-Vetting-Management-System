98c222c0cff494620122c5f1422390137f246ca9
ndean73 <djnalumpa@gmail.com>
2026-04-16 23:03:31 +0200

    Includes functional client interface

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,8 +10,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7056/") });
+builder.Services.AddHttpClient("SystemApiClient", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7056/");
+});
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7056/") });
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<GetHttpClient>();
--- ServerLibrary/obj/Debug/net10.0/ServerLibrary.AssemblyInfo.cs
+++ ServerLibrary/obj/Debug/net10.0/ServerLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b083d39336f34ccbd62e768a5a6370a23c456843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d6a2cb3f3fcf7d6cae3e67b93d13c9011ff1e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
